ab034fc356ca55978a4a886abae009f346b7be46
trunggloll <23521687@gm.uit.edu.vn>
2025-05-09 18:49:40 +0700

    Update admin packages and components

--- admin/src/router/router.jsx
+++ admin/src/router/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router"; // use react-router-dom, not just "react-router"
+import { createBrowserRouter } from "react-router-dom";
 
 import MainLayout from "../layouts/mainLayout";
 import ProductsPage from "../pages/products/Product.Page";
